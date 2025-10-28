b3608281240e31f9de78f3b9d1e4746c1b652cce
menaemg <abdalmonaem.gamal@gmail.com>
2025-10-28 17:11:21 +0300

    structure project and add martial design  and tailwindcss

---
+++ src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        // Handle errors globally
+        console.error('HTTP Error:', error);
+        return throwError(error);
+      })
+    );
+  }
+}
---
+++ src/app/core/services/cart.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  private cartItems: any[] = [];
+  private cartSubject = new BehaviorSubject<any[]>(this.cartItems);
+
+  getCart() {
+    return this.cartSubject.asObservable();
+  }
+
+}
