c1caf50d9900b4d2cb26eadf9df119509140b98b
menaemg <abdalmonaem.gamal@gmail.com>
2025-11-16 18:28:48 +0200

    add ngx-toastr for notifications and implement response popup interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,22 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { responsePopupInterceptor } from './core/interceptors/response-popup.interceptor';
+
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(),
+
+
+    importProvidersFrom(MatSnackBarModule),
+
+    provideHttpClient(
+      withInterceptors([
+        responsePopupInterceptor
+      ])
+    )
+  ]
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/**/*.{html,ts}", 
+    "./src/**/**/*.{html,ts,css}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
+  safelist: ['success-snackbar', 'error-snackbar', 'info-snackbar'],
 }
 
